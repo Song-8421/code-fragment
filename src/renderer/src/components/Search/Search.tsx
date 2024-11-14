ecc988d952884aa0fa4af5ccd8586440911a90c4
huanlong <3025897003@qq.com>
2024-11-14 11:00:20 +0800

    创建Search组件并关闭 eslint 对于组件函数返回值要求的提示

---
+++ src/renderer/src/components/Search/Search.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+function Search() {
+  const [versions] = useState(window.electron.process.versions);
+
+  return (
+    <>
+      511
+    </>
+  );
+}
+
+export default Search;
