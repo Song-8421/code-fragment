f5686e1b0e22d8f60176a2a5876575c559b284e3
huanlong <3025897003@qq.com>
2024-11-15 11:36:25 +0800

    绑定键盘按下事件与认识useEffect

--- src/renderer/src/components/Results/index.tsx
+++ src/renderer/src/components/Results/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useCode from "@renderer/hooks/useCode";
 
 const Results = () => {
   const { data } = useCode();
+  const { currentIndex, setCurrentIndex } = useState(0);
+  const handleKeyDown = (e: KeyboardEvent) => {
+    console.log("触发", e.key);
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [data]);
+
   return (
     <main className = "bg-slate-50 p-6 rounded-bl-lg rounded-br-lg -mt-6">
+      {currentIndex}
       {data.map((item) => (
         <div key = {item.id} className = "text-slate-500 truncate mb-4">
           {item.content}
